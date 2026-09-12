240a672ac2dd14b60fce6da55cbdd43afc74f02d
hixianfu <2357823678@qq.com>
2026-09-12 17:37:12 +0800

    feat: polish homepage visual components

--- components/home/FeaturedArticles.tsx
+++ components/home/FeaturedArticles.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+﻿import Link from "next/link";
 import Image from "next/image";
 import type { Locale } from "@/lib/i18n/config";
 import { localizedHref } from "@/lib/i18n/routing";
